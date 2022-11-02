0ab0cfd527373ba2feac51381c88bef28b5f35c2
JelsinPalomino <jstpalomino@hotmail.com>
2022-11-02 16:53:54 -0500

    Opción 2 / Tarea 1

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -10,18 +10,25 @@ class Libro:
         self.autor = autor
 
     def open_csv(self):
-        with open("libros.csv", "r", encoding='utf-8') as f:
+        with open('libros.csv', 'r', encoding='utf-8') as f:
             book = csv.reader(f)
             books = [row for row in book] 
-            print(books)
-        return books
+            return books
 
-    def show_books(self, books):
-        print(books)
-        libros = [row[1] for row in self.books]
+    def show_books(books):
+        listBooks = books.open_csv()
+        libros = [row[1] for row in listBooks]
         print(libros)
 
+    def search_isbn_titulo(books):
+        listBooks = books.open_csv()
+        print(f"Se puede realizar la busqueda por ISBN o por el título de los libros")
+        selection = input("Ingrese el ISBN o título a buscar: ")
+        for i in listBooks:
+            if str(i[1]) == selection:
+                print(f"{i[0]} - {i[1]} - {i[2]} - {i[3]} - {i[4]} - {i[5]}")
 
 q = Libro()
 q.open_csv()
 q.show_books()
+q.search_isbn_titulo()
