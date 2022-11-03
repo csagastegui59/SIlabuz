fe688e77086e5ec26e1da044063ace3027deae24
9-natalia <natalia16845@gmail.com>
2022-11-02 22:50:19 -0500

    seleccion

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -17,3 +17,60 @@ def menu():
         Opción 11: Salir.
         ''')
         seleccion = int(input("Eliga una opción: "))
+        if seleccion == 1:
+            leer_archivo()
+        if seleccion == 2:
+            listar()
+        if seleccion == 3: 
+            agregar()
+        if seleccion == 4:
+            eliminar()
+        if seleccion == 5:
+            buscar_isbn_o_titulo()
+        if seleccion == 6:
+            ordenar_titulo()
+        if seleccion == 7:
+            buscar_autor_editorial_o_genero()
+        if seleccion == 8:
+            buscar_numero_de_autores()
+        if seleccion == 9:
+            editar_o_actualizar()
+        if seleccion == 10:
+            guardar()
+        if seleccion == 11:
+            salir()
+    
+def leer_archivo():
+    pass
+
+def listar():
+    pass
+
+def agregar():
+    pass
+
+def eliminar():
+    pass
+
+def buscar_isbn_o_titulo():
+    pass
+
+def ordenar_titulo():
+    pass
+
+def buscar_autor_editorial_o_genero():
+    pass
+
+def buscar_numero_de_autores():
+    pass
+
+def editar_o_actualizar():
+    pass
+
+def guardar():
+    pass
+
+def salir():
+    pass
+
+menu() 
